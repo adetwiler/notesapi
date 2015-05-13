0367bf43e442a7661add245915a076dd3f19b45a
Andrew Detwiler <adetwiler@adidamnetworks.com>
2015-05-13 06:43:22 -0700

    Cleaning up note router variables

--- api/note/index.js
+++ api/note/index.js
@@ -1,9 +1,8 @@
 'use strict';
 
-var express = require('express');
-var controller = require('./note.controller');
-
-var router = express.Router();
+var express = require('express')
+    , controller = require('./note.controller')
+    , router = express.Router();
 
 router.get('/', controller.index);
 router.get('/:id', controller.show);
